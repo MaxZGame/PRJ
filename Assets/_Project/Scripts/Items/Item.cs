c14ad9e0eabb7703c8d14126c259c1645be67b0e
Максим <telefonflymax@gmail.com>
2025-08-10 15:07:48 +0300

    v.0.10c - Удаление выбранного предмета со сцены - Переключение сцен - Вызов панели при выборе предмета

--- Assets/_Project/Scripts/Items/Item.cs
+++ Assets/_Project/Scripts/Items/Item.cs
@@ -3,10 +3,61 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class Item : MonoBehaviour
 {
+    [SerializeField, Header("Это интерактивный объект?")]
+    private bool isInteractive = false;
+
+    [SerializeField, Header("===Если интерактивный, то вставляем всё ниже===")]
+    private GameObject objInteractive;
+
+    private void OnEnable()
+    {
+        if (objInteractive != null && objInteractive.activeSelf == true)
+        {
+            objInteractive.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        CloseInteractiveObj();
+    }
+
     void OnMouseDown()
     {
         Debug.Log("Кликнул по объекту");
-        Destroy(gameObject);
+        if (isInteractive)
+        {
+            objInteractive.SetActive(true);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
+    private void CloseInteractiveObj()
+    {
+        if (objInteractive != null && objInteractive.activeSelf == true)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                objInteractive.SetActive(false);
+            }
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (isInteractive)
+        {
+            if (objInteractive == null)
+            {
+                Debug.LogError($"[Item,Validate]Нет интерактивной панели для интерактивного объекта!{gameObject.name}");
+            }
+        }
+        else
+        {
+            objInteractive = null;
+        }
+    }
 }
--- Assets/_Project/Scripts/Items/ItemSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item", menuName = "Create Item", order = 1)]
-public class ItemSO : ScriptableObject
-{
-
-}
