f9008a6829e69fd603a2f7e29d70d9993cf3523b
Максим <telefonflymax@gmail.com>
2025-09-03 19:04:24 +0300

    v.0.70c Новое: -Добавлена мини-игра "Собери образ"

--- Assets/_Project/Scripts/SYSTEM/RoomsController.cs
+++ Assets/_Project/Scripts/SYSTEM/RoomsController.cs
@@ -11,6 +11,13 @@ public class RoomsController : MonoBehaviour
     //Флажки
     private bool isFirstStart = true;
 
+
+    //ДЛЯ ТЕСТОВ
+    [SerializeField, Header("Тестовый режим")]
+    private bool isTest = false;
+    [SerializeField, Header("Текст оповещения о тесте")]
+    private GameObject testText;
+
     private void Awake()
     {
         Instance = this;
@@ -23,14 +30,38 @@ public class RoomsController : MonoBehaviour
 
     public void FirstStart()
     {
-        if (isFirstStart)
+        if (isFirstStart && !isTest)
         {
+            if (testText.activeSelf)
+            {
+                testText.SetActive(false);
+            }
             startRoom.SetActive(true);
             isFirstStart = false;
         }
         else
         {
+            if (isTest)
+            {
+                isFirstStart = false;
+                if (!testText.activeSelf)
+                {
+                    testText.SetActive(true);
+                }
+            }
             Debug.Log("Это не первый запуск");
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (!isTest)
+        {
+            testText.gameObject.SetActive(false);
+        }
+        if (isTest)
+        {
+            testText.gameObject.SetActive(true);
         }
     }
 }
